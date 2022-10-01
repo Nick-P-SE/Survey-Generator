2e1088ef2850d24700fea48116736ff215a1f998
Nick Parks <nchlsparks@gmail.com>
2022-10-01 17:09:35 -0400

    messing around nothing serious

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const quizRoutes = require("./routes/quizzes");
 const questionRoutes = require("./routes/questions");
 const answerRoutes = require("./routes/answers");
 
+
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
@@ -57,6 +58,7 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
+// app.use("/faqs", mainRoutes)
 app.use("/quiz", quizRoutes);
 app.use("/question", questionRoutes);
 app.use("/answer", answerRoutes);
