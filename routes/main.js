2ca4f4eaa3188e3e21002b48bb648c3fd3e5550f
Nick Parks <nchlsparks@gmail.com>
2022-10-07 17:11:16 -0400

    updated routes

--- routes/main.js
+++ routes/main.js
@@ -7,6 +7,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
+router.get("/faqs", homeController.getFaqs)
 router.get("/profile", ensureAuth, quizzesController.getProfile);
 router.get("/feed", ensureAuth, quizzesController.getFeed);
 router.get("/login", authController.getLogin);
